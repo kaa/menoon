791376d7c88682cfdf62ab080785ed237338abac
Joakim Beijar <jbeijar@mios.fi>
2011-01-05 18:31:10 +0200

    Moving to VS and local jQuery

---
+++ www/static/Log.js
@@ -0,0 +1,25 @@
+function Log(level) {
+	Log.level = level||1;
+	$.jGrowl.defaults.closer = false
+	$.jGrowl.defaults.closeTemplate = ""
+}
+Log.error = function() {
+	if(Log.level<1) return
+	console.log(arguments)
+	$.jGrowl(Array.prototype.slice.call(arguments).join(" "),{theme:"error"})
+}
+Log.warn = function() {
+	if(Log.level<2) return
+	console.log(arguments)
+	$.jGrowl(Array.prototype.slice.call(arguments).join(" "),{theme:"warn"})
+}
+Log.message = function() {
+	if(Log.level<3) return
+	console.log(arguments)
+	$.jGrowl(Array.prototype.slice.call(arguments).join(" "),{theme:"message"})
+}
+Log.verbose = function() {
+	if(Log.level<4) return
+	console.log(arguments)
+	$.jGrowl(Array.prototype.slice.call(arguments).join(" "),{theme:"verbose"})
+}
